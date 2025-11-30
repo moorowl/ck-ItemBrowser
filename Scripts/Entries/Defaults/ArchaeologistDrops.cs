0172ca7385ff86b20e2c1a8f15bedc505b4cd178
moorowl <moorowl@outlook.com>
2025-11-30 19:56:26 +0000

    Add more info for ArchaeologistDrops

--- Scripts/Entries/Defaults/ArchaeologistDrops.cs
+++ Scripts/Entries/Defaults/ArchaeologistDrops.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using ItemBrowser.Utilities;
 using UnityEngine;
 
@@ -7,14 +8,18 @@ namespace ItemBrowser.Entries.Defaults {
 		public override ObjectEntryCategory Category => new("ItemBrowser:ObjectEntry/ArchaeologistDrops", ObjectID.WallStoneBlock, 5100);
 		
 		public ObjectID Result { get; set; }
-		public float Chance { get; set; }
+		public (float Min, float Max) Chance { get; set; }
 		
 		public class Provider : ObjectEntryProvider {
 			public override void Register(ObjectEntryRegistry registry, List<(ObjectData ObjectData, GameObject Authoring)> allObjects) {
+				var chanceAtMin = Manager.mod.SkillTalentsTable.skillTalentTrees.SelectMany(tree => tree.skillTalents)
+					.FirstOrDefault(talent => talent.givesCondition == ConditionID.ChanceForRandomLootFromWall).conditionValuePerPoint / 100f;
+				var chanceAtMax = chanceAtMin * Constants.kSkillPointsPerTalentPoint;
+				
 				foreach (var drop in LootUtils.GetLootTableContents(LootTableID.ArcheologistWallLoot)) {
 					registry.Register(ObjectEntryType.Source, drop.ObjectId, 0, new ArchaeologistDrops {
 						Result = drop.ObjectId,
-						Chance = drop.Chance
+						Chance = (drop.Chance * chanceAtMin, drop.Chance * chanceAtMax)
 					});
 				}
 			}
--- Scripts/Entries/Defaults/ArchaeologistDropsDisplay.cs
+++ Scripts/Entries/Defaults/ArchaeologistDropsDisplay.cs
@@ -1,5 +1,7 @@
-﻿using ItemBrowser.Utilities;
+﻿using System.Linq;
+using ItemBrowser.Utilities;
 using ItemBrowser.UserInterface.Browser;
+using PugMod;
 using UnityEngine;
 
 namespace ItemBrowser.Entries.Defaults {
@@ -8,13 +10,33 @@ namespace ItemBrowser.Entries.Defaults {
 		private BasicItemSlot resultSlot;
 		[SerializeField]
 		private PugText chanceText;
-		
+
 		public override void RenderSelf() {
+			RenderBody();
+			RenderMoreInfo();
+		}
+
+		private void RenderBody() {
 			resultSlot.DisplayedObject = new DisplayedObject.Static(new ObjectDataCD {
 				objectID = Entry.Result
 			});
-			
-			chanceText.Render(UserInterfaceUtils.FormatChance(Entry.Chance) + "%");
+			chanceText.Render($"{UserInterfaceUtils.FormatChance(Entry.Chance.Min)}-{UserInterfaceUtils.FormatChance(Entry.Chance.Max)}%");
+		}
+		
+		private void RenderMoreInfo() {
+			MoreInfo.AddLine(new TextAndFormatFields {
+				text = "ItemBrowser:MoreInfo/ArchaeologistDrops_0",
+				color = UserInterfaceUtils.DescriptionColor
+			});
+			MoreInfo.AddLine(new TextAndFormatFields {
+				text = $"ItemBrowser:MoreInfo/ArchaeologistDrops_1",
+				formatFields = new[] {
+					UserInterfaceUtils.FormatChance(Entry.Chance.Min),
+					UserInterfaceUtils.FormatChance(Entry.Chance.Max)
+				},
+				dontLocalizeFormatFields = true,
+				color = UserInterfaceUtils.DescriptionColor
+			});
 		}
 	}
 }
