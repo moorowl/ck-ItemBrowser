fa87812a0c71e1a0d8727593e5f9078bc48b5d03
moorowl <moorowl@outlook.com>
2025-12-17 19:07:43 +0000

    Cleanup

--- Scripts/Api/Entries/ObjectEntryDisplayBase.cs
+++ Scripts/Api/Entries/ObjectEntryDisplayBase.cs
@@ -1,20 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
 
 namespace ItemBrowser.Api.Entries {
 	public abstract class ObjectEntryDisplayBase : UIelement {
-		[SerializeField]
-		[HideInInspector]
-		private UIelement[] topMostUIElements;
-		[SerializeField]
-		[HideInInspector]
-		private UIelement[] bottomMostUIElements;
-		
-		public IEnumerable<UIelement> TopMostUIElements => topMostUIElements;
-		public IEnumerable<UIelement> BottomMostUIElements => bottomMostUIElements;
-		
 		public abstract Type AssociatedEntry { get; }
 		
 		public abstract void SetEntry(ObjectEntry entry, ObjectData objectData);
@@ -24,10 +12,5 @@ namespace ItemBrowser.Api.Entries {
 		public abstract void Render();
 
 		public abstract float CalculateHeight();
-
-		private void OnValidate() {
-			topMostUIElements = GetComponentsInChildren<UIelement>().Where(element => element != this && element.topUIElements.Count == 0).ToArray();
-			bottomMostUIElements = GetComponentsInChildren<UIelement>().Where(element => element != this && element.bottomUIElements.Count == 0).ToArray();
-		}
 	}
 }
